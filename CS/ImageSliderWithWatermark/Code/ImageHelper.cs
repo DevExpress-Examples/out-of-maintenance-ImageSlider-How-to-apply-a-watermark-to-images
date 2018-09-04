b9c97ce0f01e0cd68de8f3d86135740927d7cd42
Stanislav Gurov (DevExpress) <gurov.stanislav@corp.devexpress.com>
2018-09-04 23:39:47 +0300

    add CS example content

---
+++ CS/ImageSliderWithWatermark/Code/ImageHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ImageSliderWithWatermark.Code
+{
+    public class ImageHelper
+    {
+        private static Image GetImageFromUrl(string url)
+        {
+            var image = Image.FromFile(HttpContext.Current.Server.MapPath(url));
+            return image;
+        }
+
+        private static byte[] ImageToByteArray(Image image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, image.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        public static byte[] GetImageWithWatermark(string url, string text, string fontName)
+        {
+            var image = GetImageFromUrl(url);
+            PhotoUtils.DrawWatermarkText(image, text, fontName);
+            return ImageToByteArray(image);
+        }
+    }
+}
