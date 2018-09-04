db0ff4141f472dcba964ead9469efcbb33d76094
Stanislav Gurov (DevExpress) <gurov.stanislav@corp.devexpress.com>
2018-09-04 23:40:49 +0300

    add VB example content

---
+++ VB/ImageSliderWatermark/Controllers/HomeController.vb
@@ -0,0 +1,13 @@
+Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+    <HttpPost>
+    Public Function Index(ByVal applyWatermarkCheckBox As Boolean?) As ActionResult
+        ViewData("isWatermarkApplied") = applyWatermarkCheckBox
+        Return View()
+    End Function
+End Class
---
+++ VB/ImageSliderWatermark/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.vb" Inherits="ImageSliderWatermark.MvcApplication" Language="VB" %>
